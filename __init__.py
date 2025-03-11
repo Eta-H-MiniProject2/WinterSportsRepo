928979e48017c85eb29464d72dc2ce83cabdb6fc
Niina Ahola <niina1102@hotmail.com>
2025-03-11 13:30:18 +0200

    Data combining done. Kaurila, Messilä and Mustavaara not working yet

--- __init__.py
+++ __init__.py
@@ -1,5 +1,10 @@
-import data_combining_functions
+import combine_data
 
-data_combining_functions.combine_cloud_coverage()
+#data_combining_functions.combine_cloud_coverage()
 #data_combining_functions.combine_snow_depth_and_temperature_files()
+
+locations = ['Kasurila','Levi','Luosto','Messila','Mustavaara','Ounasvaara','Purnu','Ruka','Ruunarinteet','Salla']
+
+for location in locations:
+    combine_data.combine_data_by_location(location)
 
